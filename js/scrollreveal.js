9ce0a91af9d19c91e7bee2e9b6f3cfd5b04c469f
tingjou47 <ttseng1@hawk.iit.edu>
2020-01-17 21:08:36 -0500

    hw 19

---
+++ js/scrollreveal.js
@@ -0,0 +1,29 @@
+// ScrollReveal (https://github.com/jlmakes/scrollreveal)
+
+(function scrollReveal() {
+    window.sr = ScrollReveal();
+    
+    sr.reveal('.card', {
+      duration   : 600,
+      distance   : '20px',
+      easing     : 'ease-out',
+      origin     : 'bottom',
+      reset      : true,
+      scale      : 1,
+      viewFactor : 0,
+      afterReveal  : revealChildren,
+    }, 150);
+    
+      var revealChildren = sr.reveal('.card-title, .card-text', {
+      duration   : 500,
+      scale      : 1,
+      distance   : '20px',
+      origin     : 'bottom',
+      reset      : true,
+      easing     : 'ease-out',
+      viewFactor : 1,
+    }, 75);
+  })();
+  
+
+  
